8571b6550614f44b157d9c273c2729463d9f9ba3
sergeogah <sergeleo14@yahoo.fr>
2026-07-07 17:10:06 +0000

    aze

--- devices/views.py
+++ devices/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
